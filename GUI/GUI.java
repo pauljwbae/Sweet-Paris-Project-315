025636445c1a6d498bade09c8c70fb94c3b915d0
Sarika <sarikavenkat21@gmail.com>
2023-10-12 14:30:35 -0500

    added customer name

--- GUI/GUI.java
+++ GUI/GUI.java
@@ -1,96 +1,141 @@
-import java.sql.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.*;
 
-/*
-  TODO:
-  1) Change credentials for your own team's database
-  2) Change SQL command to a relevant query that retrieves a small amount of data
-  3) Create a JTextArea object using the queried data
-  4) Add the new object to the JPanel p
-*/
-
 public class GUI extends JFrame implements ActionListener {
-    static JFrame f;
+    private static JFrame frame;
+    private static JTextArea orderTextArea;
+    private static List<MenuItem> menuData;
+    private static List<MenuItem> selectedItems;
+    private static double totalOrderPrice;
+    private JTextField customerNameField;
 
-    public static void main(String[] args)
-    {
-      //Building the connection
-      Connection conn = null;
-      //TODO STEP 1 (see line 7)
-      String database_name = "csce315_970_03db";
-      String database_user = "csce315_970_03user";
-      String database_password = "fourfsd";
-      String database_url = String.format("jdbc:postgresql://csce-315-db.engr.tamu.edu/%s", database_name);
-      try {
-        conn = DriverManager.getConnection(database_url, database_user, database_password);
-      } catch (Exception e) {
-        e.printStackTrace();
-        System.err.println(e.getClass().getName()+": "+e.getMessage());
-        System.exit(0);
-      }
-      JOptionPane.showMessageDialog(null,"Opened database successfully");
-
-      String name = "";
-      try{
-        //create a statement object
-        Statement stmt = conn.createStatement();
-        //create a SQL statement
-        //TODO Step 2 (see line 8)
-        String sqlStatement = "select name from customers";
-        //send statement to DBMS
-        ResultSet result = stmt.executeQuery(sqlStatement);
-        while (result.next()) {
-          name += result.getString("name")+"\n";
-        }
-      } catch (Exception e){
-        JOptionPane.showMessageDialog(null,"Error accessing Database.");
-      }
-      // create a new frame
-      f = new JFrame("DB GUI");
-
-      // create a object
-      GUI s = new GUI();
-
-      // create a panel
-      JPanel p = new JPanel();
-
-      JButton b = new JButton("Close");
-
-      // add actionlistener to button
-      b.addActionListener(s);
-
-      //TODO Step 3 (see line 9)
-      
-
-      //TODO Step 4 (see line 10)
-
-      // add button to panel
-      p.add(b);
-
-      // add panel to frame
-      f.add(p);
-
-      // set the size of frame
-      f.setSize(400, 400);
-
-      f.setVisible(true);
-
-      //closing the connection
-      try {
-        conn.close();
-        JOptionPane.showMessageDialog(null,"Connection Closed.");
-      } catch(Exception e) {
-        JOptionPane.showMessageDialog(null,"Connection NOT Closed.");
-      }
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+        gui.menuData = readMenuItemsFromCSV("20items.csv"); // Initialize menuData
+        gui.createGUI();
     }
 
-    // if button is pressed
+    public void createGUI() {
+        frame = new JFrame("Order Entry System");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create a panel for menu items
+        JPanel menuPanel = new JPanel();
+
+        // Create a text area for entering orders
+        orderTextArea = new JTextArea(10, 40);
+        JScrollPane scrollPane = new JScrollPane(orderTextArea);
+
+        // Create a panel for customer name and order submission
+        JPanel controlPanel = new JPanel();
+        customerNameField = new JTextField(20);
+        JButton submitOrderButton = new JButton("Submit Order");
+        submitOrderButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                submitOrder();
+            }
+        });
+
+        controlPanel.add(new JLabel("Customer Name: "));
+        controlPanel.add(customerNameField);
+        controlPanel.add(submitOrderButton);
+
+        // Create buttons for each menu item
+        JPanel itemsPanel = new JPanel();
+        for (MenuItem menuItem : menuData) {
+            JButton itemButton = new JButton(menuItem.getName() + " - $" + menuItem.getPrice());
+            itemButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    addToOrder(menuItem);
+                }
+            });
+            itemsPanel.add(itemButton);
+        }
+
+        // Add components to the frame
+        frame.add(menuPanel);
+        frame.add(scrollPane);
+        frame.add(controlPanel);
+        frame.add(itemsPanel);
+
+        // Set layout and size
+        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+    }
+
+    private void submitOrder() {
+        String customerName = customerNameField.getText();
+        // You can insert the order into the database with customerName and selectedItems
+        // Display a confirmation message to the cashier
+        JOptionPane.showMessageDialog(null, "Order submitted for " + customerName + ". Total price: $" + totalOrderPrice);
+
+        // Clear selected items, total price, and customer name
+        selectedItems.clear();
+        totalOrderPrice = 0.0;
+        orderTextArea.setText("");
+        customerNameField.setText("");
+    }
+
+    private void addToOrder(MenuItem menuItem) {
+        selectedItems.add(menuItem);
+        totalOrderPrice += menuItem.getPrice();
+        orderTextArea.append(menuItem.getName() + " - $" + menuItem.getPrice() + "\n");
+    }
+
+    // MenuItem class to store menu items with name and price
+    static class MenuItem {
+        private String name;
+        private double price;
+
+        public MenuItem(String name, double price) {
+            this.name = name;
+            this.price = price;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        @Override
+        public String toString() {
+            return name + " - $" + price;
+        }
+    }
+
+    private static List<MenuItem> readMenuItemsFromCSV(String filename) {
+      List<MenuItem> items = new ArrayList<>();
+      try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+          String line;
+          // Skip the header line
+          br.readLine();
+          while ((line = br.readLine()) != null) {
+              String[] data = line.split(",");
+              String name = data[1];
+              double price = Double.parseDouble(data[7]); // Assuming price is in the eighth column
+              items.add(new MenuItem(name, price));
+          }
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+      return items;
+    }
+
     public void actionPerformed(ActionEvent e)
     {
         String s = e.getActionCommand();
         if (s.equals("Close")) {
-            f.dispose();
+            frame.dispose();
         }
     }
 }
